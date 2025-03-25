21860a8a73db7f417acbe3817f6941b1001f31ca
DinoCustovic-dev <146726440+DinoCustovic-dev@users.noreply.github.com>
2025-03-25 06:47:15 +0000

    feat: lastone

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -120,7 +120,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
 export const useCart = () => {
   const context = useContext(CartContext);
   if (!context) {
-    throw new Error('useCart must be used within a CartProvider');
+    throw Error('useCart must be used within a CartProvider');
   }
   return context;
 };
